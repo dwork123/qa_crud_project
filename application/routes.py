d652a8c93b51f57e7fbc0d571c3e6586e2658e9d
dond45 <dondre.ty20@hotmail.com>
2021-11-10 23:37:48 +0000

    added input forms for csutomers and orders

--- application/routes.py
+++ application/routes.py
@@ -18,8 +18,8 @@ def editrecordform(cust_id):
     cust = customer.query.filter_by(cust_id=cust_id).first()
 
     if request.method == 'POST':
-        cust.name = form.cust_first_name.data
-        cust.last_name = form.cust_last_name.data
+        cust.name = form.first_name.data
+        cust.last_name = form.last_name.data
         cust.address = form.address.data
         cust.phone_no = form.phone_no.data
         cust.e_mail = form.e_mail.data
@@ -27,6 +27,26 @@ def editrecordform(cust_id):
         db.session.commit()
         return redirect("/")
     return render_template('editform.html', form=form)
+
+# update order
+@app.route("/editrecord/<int:order_id>", methods=['GET', 'POST'])
+
+def editrecordform(customer_id):
+    form = update_order
+    order = order.query.filter_by(order_id=order_id).first()
+
+        if request.method == 'POST':
+        cust.name = form.item.data
+        cust.last_name = form.quantity.data
+        cust.address = form.price.data
+
+    db.session.commit()
+        return redirect("/")
+    return render_template('editorderform.html', form=form)
+
+
+
+
 
 
 @app.route("/filterrecords",methods=["POST"])
@@ -50,23 +70,42 @@ def saverecord():
         cust.phone_no = form.phone_no.data
         cust.e_mail = form.e_mail.data
 
+        new_cust = customer(first_name=first_name, last_name=last_name, address=address, phone_no=phone_no, e_mail=e_mail)
         db.session.add(new_cust)
         db.session.commit()
         return redirect("/")
         return render_template("inputform.html", form=form)
 
 
+@app.route("/saverecord",methods=["GET","POST"])
+def saverecord():
+    form = add_order()
+    if request.method == 'POST':
 
-#update with database info
+
+
 @app.route("/personaldetails/<int:customer_id>")
-def personalinformation(cust):
-	data = customer.query.filter_by(cust=cust).first()
-	return render_template("personalinfo.html",record=data)
+def personalinformation(customer_id):
+	data = customer.query.filter_by(customer_id=customer_id).first()
+	return render_template("customerinfo.html", record=data)
+
+@app.route("/personaldetails/<int:order_id>")
+def personalinformation(order_id):
+	data = order.query.filter_by(order_id=orderr_id).first()
+	return render_template("orderinfo.html", record=data)
 
 
+#delete functions
 @app.route("/deletecustomer/<int:customer_id>")
-def deletecustomer(cust):
-    cust = cust.query.filter_by(cust=cust).first()
+def deletecustomer(customer_id):
+    cust = cust.query.filter_by(customer_id=custumer_id).first()
     db.session.delete(cust)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/deleteorder/<int:order_id>")
+def deleteorder(order_id):
+    ord = order.query.filter_by(order_id=order_id).first()
+    db.session.delete(ord)
     db.session.commit()
     return redirect("/")
