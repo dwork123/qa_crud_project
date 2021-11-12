1741b1cc6b40966bf787b61a571e5d63f870727d
dwork123 <dondre.work123@gmail.com>
2021-11-12 14:33:01 +0000

    QCP-3 #in-progress created model databases

--- application/models.py
+++ application/models.py
@@ -1,20 +1,20 @@
 from application import db
 
 class customer(db.Model):
-    customer_id = db.column(db.Integer, primary_key= True)
-    first_name= db.column(db.String(50))
-    last_name= db.column(db.String(50))
-    address= db.columndb.Column(db.char(255))
-    phone_no= db.column(db.Integer)
-    email_address= db.column(db.String(50))
+    customer_id = db.Column(db.Integer, primary_key= True)
+    first_name= db.Column(db.String(50))
+    last_name= db.Column(db.String(50))
+    address= db.Column(db.String(255))
+    phone_no= db.Column(db.Integer)
+    email_address= db.Column(db.String(50))
     customer_orders= db.relationship('order', backref= 'customer')
 
 class order(db.Model):
-    order_id = db.column(db.Integer, primary_key= True)
-    item= db.column(db.String(50))
-    quantity= db.column(db.Integer)
-    price= db.column(db.Integer)
-    customer_id= db.column(db.Integer, db.foreign_key('customer.customer_id'))
+    order_id = db.Column(db.Integer, primary_key= True)
+    item= db.Column(db.String(50))
+    quantity= db.Column(db.Integer)
+    price= db.Column(db.Integer)
+    customer_id= db.Column(db.Integer, db.ForeignKey('customer.customer_id'))
     
     # def __repr__(self):
     #     return f"{self.quantity} {self.item} {self.price} {self.customer_id}"
